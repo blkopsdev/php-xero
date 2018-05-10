08d6404660a04e29ae5ac18dfa1658cc3b2244c0
Michael Calcinai <michael@calcin.ai>
2018-05-10 18:19:31 +1200

    Basic routing

---
+++ app/Controllers/ApplicationController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package    xero-php-sample-app
+ * @author     Michael Calcinai <michael@calcin.ai>
+ */
+
+namespace App\Controllers;
+
+
+class ApplicationController extends BaseController
+{
+
+}
---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    xero-php-sample-app
+ * @author     Michael Calcinai <michael@calcin.ai>
+ */
+
+namespace App\Controllers;
+
+
+abstract class BaseController
+{
+    public function __construct()
+    {
+
+    }
+}
