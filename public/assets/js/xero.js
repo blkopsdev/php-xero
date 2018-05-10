663dc753292a902d401457bccaaee3af0095a563
Michael Calcinai <michael@calcin.ai>
2018-05-10 18:20:32 +1200

    Removed unnecessary handlebars template

---
+++ public/assets/js/xero.js
@@ -0,0 +1,111 @@
+Handlebars.getTemplate = function (name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        $.get({
+            url: 'assets/templates/' + name + '.handlebars',
+            success: function (data) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                Handlebars.templates[name] = Handlebars.compile(data);
+            },
+            async: false
+        });
+    }
+    return Handlebars.templates[name];
+};
+
+var endpoint = [
+    {name: "Accounts", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}, {name: "Archive"}]},
+    {name: "BankTransactions", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}]},
+    {name: "BankTransfers", action: [{name: "Create"}, {name: "Read"}]},
+    {name: "BrandingThemes", action: [{name: "Read"}]},
+    {name: "Contacts", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Archive"}]},
+    {name: "ContactGroups", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Archive"}]},
+    {
+        name: "CreditNotes",
+        action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}, {name: "Allocate"}, {name: "Refund"}, {name: "Void"}]
+    },
+    {name: "Currencies", action: [{name: "Read"}]},
+    {name: "Employees", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}]},
+    {name: "ExpenseClaims", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}]},
+    {name: "Invoices", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}, {name: "Void"}]},
+    {name: "InvoiceReminders", action: [{name: "Read"}]},
+    {name: "Items", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}]},
+    {name: "Journals", action: [{name: "Read"}]},
+    {name: "LinkedTransactions", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}]},
+    {name: "ManualJournals", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}]},
+    {name: "Organisations", action: [{name: "Read"}]},
+    {name: "Overpayments", action: [{name: "Create"}, {name: "Read"}, {name: "Allocate"}, {name: "Refund"}]},
+    {name: "Payments", action: [{name: "Create"}, {name: "Read"}, {name: "Delete"}]},
+    {name: "Prepayments", action: [{name: "Create"}, {name: "Read"}, {name: "Allocate"}, {name: "Refund"}]},
+    {name: "PurchaseOrders", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}]},
+    {name: "Receipts", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}]},
+    {name: "RepeatingInvoices", action: [{name: "Read"}]},
+    {
+        name: "Reports",
+        action: [{name: "TenNinetyNine"}, {name: "AgedPayablesByContact"}, {name: "AgedReceivablesByContact"}, {name: "BalanceSheet"}, {name: "BankStatement"}, {name: "BankSummary"}, {name: "BudgetSummary"}, {name: "ExecutiveSummary"}, {name: "ProfitAndLoss"}, {name: "TrialBalance"}]
+    },
+    {name: "TaxRates", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}]},
+    {name: "TrackingCategories", action: [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}, {name: "Archive"}]},
+    {name: "TrackingOptions", action: [{name: "Read"}]},
+    {name: "Users", action: [{name: "Read"}]}
+];
+
+function populateAction(currEndpoint, currAction) {
+    for (var i = 0; i < endpoint.length; i++) {
+        if (endpoint[i].name == currEndpoint) {
+            temp = endpoint[i].action;
+        }
+    }
+    $("#action").children().remove();
+
+    for (var i = 0; i < temp.length; i++) {
+        if (temp[i].name == currAction) {
+            var selected = 'selected="true"';
+        } else {
+            var selected = '';
+        }
+
+        $("#action").append('<option ' + selected + ' value="' + temp[i].name + '">' + temp[i].name + '</option>');
+    }
+}
+
+function setArray(arr, match) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        if (arr[i].name === match) {
+            arr[i].selected = "selected";
+        }
+    }
+}
+
+function loadGet(appName, logoutUrl, currEndpoint, currAction) {
+
+    setArray(endpoint, currEndpoint);
+
+    var template = Handlebars.getTemplate('container');
+    $("#req").html(template({
+        name: appName,
+        logoutUrl: logoutUrl
+    }));
+
+    template = Handlebars.getTemplate('options');
+    document.querySelector("#endpoint").innerHTML = template(endpoint);
+
+    var action = [{name: "Create"}, {name: "Read"}, {name: "Update"}, {name: "Delete"}];
+    document.querySelector("#action").innerHTML = template(action);
+
+    populateAction(currEndpoint, currAction);
+
+    $("#endpoint").on("change", function () {
+        if (currAction !== $("#action").val()) {
+            currAction = $("#action").val();
+        }
+        populateAction($("#endpoint").val(), currAction);
+    });
+}
+
+function loadIndex(appName, requestTokenUrl) {
+    var template = Handlebars.getTemplate('index');
+    document.querySelector("#req").innerHTML = template(data);
+}
--- public/index.php
+++ public/index.php
@@ -1,18 +1,32 @@
 <html>
-	<head>
-		<title>xero-php sample app</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.min.js"  crossorigin="anonymous"></script>
-		<script src="xero-sdk-ui/xero.js" crossorigin="anonymous"></script>
-		<script type="text/javascript">
-		   	document.addEventListener("DOMContentLoaded", function() {
-	    		loadIndex("xero-php sample app",'request_token.php');
-			});
-   		</script>
-	</head>
-	<body>
-		<div id="req" class="container"></div>
-	</body>
+<head>
+    <title>xero-php Sample App</title>
+
+    <base href="/">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.min.js" crossorigin="anonymous"></script>
+
+    <script src="assets/js/xero.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="req" class="container">
+    <div class="row">
+        <div class="col">
+            <h2>xero-php Sample App</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a href="oauth/request-token"><img src="assets/images/connect_xero_button_blue.png"></a>
+        </div>
+    </div>
+</div>
+</body>
 </html>
